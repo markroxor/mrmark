f714d78eba65b936e67f242b4e37ec47d73cc3e2
Mohit Rathore <mrmohitrathoremr@gmail.com>
2018-06-18 14:20:15 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,26 @@
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+bot_token = '502493425:AAEhMt3wAIz6CIansaAQeq79myV8xUOznGI'
+
+def get_url(method):
+  return "https://api.telegram.org/bot{}/{}".format(bot_token,method)
+
+def process_message(update):
+    data = {}
+    data["chat_id"] = update["message"]["from"]["id"]
+    data["text"] = "I can hear you!"
+    r = requests.post(get_url("sendMessage"), data=data)
+
+@app.route("/", methods=["POST"])
+def process_update():
+    if request.method == "POST":
+        update = request.get_json()
+        print(update)
+        if "message" in update:
+            process_message(update)
+        return "ok!", 200
+if __name__ == '__main__':
+   app.run(debug = True)
